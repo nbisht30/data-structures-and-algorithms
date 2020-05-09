80bac76ff331a9f25cc4fc1eb956c8df42752541
nbisht30 <nbisht30@gmail.com>
2020-05-09 18:51:20 +0530

    LC 237 - Delete node with access to only that node

---
+++ src/leetcode/randomproblems/linkedlist/P237DeleteNodeOfLLWithAccessToOnlyThatNode.java
@@ -0,0 +1,23 @@
+package leetcode.randomproblems.linkedlist;
+// https://leetcode.com/problems/delete-node-in-a-linked-list/
+class P237DeleteNodeOfLLWithAccessToOnlyThatNode {
+
+    public void deleteNode(ListNode node) {
+        ListNode slNode = null;
+        while (node.next != null) {
+            slNode = node;
+            node.val = node.next.val;
+            node = node.next;
+        }
+        slNode.next = null;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+}
