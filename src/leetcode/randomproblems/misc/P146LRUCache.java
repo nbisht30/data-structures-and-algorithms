711af9012389da0eaf3b7827c849eb05ca27b22c
nbisht30 <nbisht30@gmail.com>
2020-05-18 19:03:25 +0530

    LC - P145 LRU Cache

---
+++ src/leetcode/randomproblems/misc/P146LRUCache.java
@@ -0,0 +1,39 @@
+package leetcode.randomproblems.misc;
+
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+
+class P146LRUCache {
+    LinkedHashMap<Integer, Integer> keyval;
+    int capacity;
+
+    public P146LRUCache(int capacity) {
+        this.capacity = capacity;
+        this.keyval = new LinkedHashMap<>(capacity);
+    }
+
+    public int get(int key) {
+        if (keyval.size() == 0 || !keyval.containsKey(key)) return -1;
+
+        int value = keyval.get(key);
+        keyval.remove(key);
+        keyval.put(key, value);
+
+        return value;
+    }
+
+    public void put(int key, int value) {
+        if (keyval.containsKey(key)) {
+            keyval.remove(key);
+        } else if (keyval.size() == this.capacity) {
+            Set<Map.Entry<Integer, Integer>> mapEntries = keyval.entrySet();
+            Iterator<Map.Entry<Integer, Integer>> itr = mapEntries.iterator();
+            int firstKey = itr.next().getKey();
+            keyval.remove(firstKey);
+        }
+        keyval.put(key, value);
+
+    }
+}
