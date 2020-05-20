7c9057d5e97e655e3e4cebef58024b79282253c5
nbisht30 <nbisht30@gmail.com>
2020-05-20 10:37:21 +0530

    LC - P20 Valid Parentheses

---
+++ src/leetcode/randomproblems/stacks/P20ValidParentheses.java
@@ -0,0 +1,49 @@
+package leetcode.randomproblems.stacks;
+
+import java.util.LinkedList;
+
+class P20ValidParentheses {
+    public boolean isValid(String s) {
+        MyStack<Character> stack = new MyStack<>();
+        
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{'){
+                stack.push(s.charAt(i));
+            }
+            else if(!stack.isEmpty()){
+                if((s.charAt(i) == ')' && stack.peek() == '(')
+                   || (s.charAt(i) == ']' && stack.peek() == '[')
+                        || (s.charAt(i) == '}' && stack.peek() == '{')) stack.pop();
+                else return false;
+            }else{
+                return false;
+            }
+        }
+        
+        return stack.isEmpty();
+    }
+    
+    
+    private class MyStack<T> {
+        LinkedList<T> stack = new LinkedList<>();
+        int size = 0;
+
+        boolean isEmpty() {
+            return size == 0;
+        }
+
+        void push(T item) {
+            this.size++;
+            this.stack.addLast(item);
+        }
+
+        T pop() {
+            this.size--;
+            return this.stack.removeLast();
+        }
+
+        T peek() {
+            return this.stack.getLast();
+        }
+    }
+}
