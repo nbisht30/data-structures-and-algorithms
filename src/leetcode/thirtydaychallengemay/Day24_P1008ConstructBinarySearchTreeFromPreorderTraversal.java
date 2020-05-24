7d8eb73f2cd2d60b4777dbfb66b13a1a0db1c708
nbisht30 <nbisht30@gmail.com>
2020-05-25 00:39:35 +0530

    LC - P101 Symmetric Tree

--- src/leetcode/thirtydaychallengemay/Day24_P1008ConstructBinarySearchTreeFromPreorderTraversal.java
+++ src/leetcode/thirtydaychallengemay/Day24_P1008ConstructBinarySearchTreeFromPreorderTraversal.java
@@ -23,7 +23,9 @@ class Day24_P1008ConstructBinarySearchTreeFromPreorderTraversal {
     public TreeNode bstFromPreorder(int[] preorder) {
         return helper(preorder, 0, preorder.length - 1);
     }
-    
+
+
+    // My implementation but this is slower as it is O(n2);
     private TreeNode helper(int[] preorder, int st, int end){ 
         if(st == end) return new TreeNode(preorder[st]);
         
@@ -46,5 +48,28 @@ class Day24_P1008ConstructBinarySearchTreeFromPreorderTraversal {
         root.right = helper(preorder, rSt, rEnd);
         
         return root;
+    }
+
+    // O(n) solution: Idea : Have upper bound for each node you're trying to construct. For left subtree the upper bound would be
+    // root's value and for right subtree the upper bound would be max upper bound.
+    // https://leetcode.com/problems/construct-binary-search-tree-from-preorder-traversal/discuss/252232/JavaC%2B%2BPython-O(N)-Solution
+    class Solution {
+        int i = 0;
+
+        public TreeNode bstFromPreorder(int[] preorder) {
+            return helper(preorder, Integer.MAX_VALUE);
+        }
+
+        private TreeNode helper(int[] preorder, int upperBound){
+            if(i == preorder.length || preorder[i] > upperBound) return null;
+
+            TreeNode root = new TreeNode(preorder[i++]);
+
+            root.left = helper(preorder, root.val);
+
+            root.right = helper(preorder, upperBound);
+
+            return root;
+        }
     }
 }
