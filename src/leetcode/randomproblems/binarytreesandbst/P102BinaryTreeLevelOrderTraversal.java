08fd8453278bd66f5f3b5bf3285f226a7db88bae
nbisht30 <nbisht30@gmail.com>
2020-05-25 09:47:18 +0530

    LC - P103 Binary Tree Zigzag Level Order Traversal

--- src/leetcode/randomproblems/binarytreesandbst/P102BinaryTreeLevelOrderTraversal.java
+++ src/leetcode/randomproblems/binarytreesandbst/P102BinaryTreeLevelOrderTraversal.java
@@ -2,6 +2,7 @@ package leetcode.randomproblems.binarytreesandbst;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 
 class P102BinaryTreeLevelOrderTraversal {
@@ -34,6 +35,33 @@ class P102BinaryTreeLevelOrderTraversal {
             list = tempList;
         }
         return mainList;
+    }
+
+    // This is my attempt when I performed revision: THIS IS MUCH MORE CLEANER! I am improving.
+    public List<List<Integer>> levelOrderRevisionAttempt(TreeNode root) {
+        if(root == null) return new ArrayList<>();
+
+        List<List<Integer>> retList = new ArrayList<>();
+        LinkedList<TreeNode> queue = new LinkedList<>();
+
+        queue.add(root);
+
+        while(!queue.isEmpty()){
+
+            List<Integer> intLevel = new ArrayList<>();
+
+            int i = 0, size = queue.size();
+            while(i < size){
+                TreeNode temp = queue.removeFirst();
+                intLevel.add(temp.val);
+                if(temp.left != null) queue.add(temp.left);
+                if(temp.right != null) queue.add(temp.right);
+                i++;
+            }
+            if(intLevel.size() > 0) retList.add(intLevel);
+        }
+
+        return retList;
     }
 
     public class TreeNode {
