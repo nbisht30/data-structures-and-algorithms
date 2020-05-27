26a372c8920f0cb7db64a75b172930a260e4681d
nbisht30 <nbisht30@gmail.com>
2020-05-27 20:51:04 +0530

    LC - P287 Find The Duplicate Number

--- src/leetcode/solved/categorized/bitmanipulation/P136SingleNumber.java
+++ src/leetcode/solved/categorized/bitmanipulation/P136SingleNumber.java
@@ -43,6 +43,15 @@ public class P136SingleNumber {
         return 2 * sumOfUniques - sumAll;
     }
 
+    /*
+    If we take XOR of zero and some bit, it will return that bit
+    a ⊕ 0 = a
+    If we take XOR of two same bits, it will return 0
+    a ⊕ a = 0
+    a ⊕ b ⊕ a=(a ⊕ a) ⊕ b = 0 ⊕ b = b
+    So we can XOR all bits together to find the unique number.
+     */
+
     public static int singleNumberUsingBitMaths(int[] nums) {
         int a = 0;
         for (int i : nums) {
