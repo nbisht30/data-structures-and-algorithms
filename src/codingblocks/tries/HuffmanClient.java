5bf19b72f554437c0a87d7adc5588cc81234c6af
nbisht30 <nbisht30@gmail.com>
2020-06-09 13:08:19 +0530

    DS - Huffman Encoder Decoder Impl | Final size reduction.

--- src/codingblocks/tries/HuffmanClient.java
+++ src/codingblocks/tries/HuffmanClient.java
@@ -1,5 +1,7 @@
 package codingblocks.tries;
 
+import java.util.BitSet;
+
 /**
  * @author Nikhil Bisht
  * @date 09-06-2020
@@ -11,6 +13,23 @@ public class HuffmanClient {
         String codedString = huffmanEncoderDecoder.encode(str);
         System.out.println(codedString);
         System.out.println(huffmanEncoderDecoder.decode(codedString));
+
+
+        // codedString currently occupies a more bytes than the orriginal string str as it has more number of characters
+        // so we need to store bits as bits, instead of storing them as characters inside a string.
+
+        BitSet bitSet = new BitSet(codedString.length());
+        for (int i = 0; i < codedString.length(); i++) {
+            if (codedString.charAt(i) == '1') {
+                bitSet.set(i);
+            }
+        }
+
+        byte[] arr = bitSet.toByteArray(); //to count number of bytes finally occupied
+        System.out.println("********************************");
+        System.out.println("Final size occupied by coded string in HDD: " + arr.length);
+        System.out.println("Final coded string: " + bitSet);
+
 
     }
 }
