4fa6c22741479ca21587e830ab2c38a1873196e6
nbisht30 <nbisht30@gmail.com>
2020-08-11 20:39:04 +0530

    LC - P437 Path Sum III

---
+++ src/leetcode/solved/categorized/binarytreesandbst/P437PathSumIII.java
@@ -0,0 +1,84 @@
+package leetcode.solved.categorized.binarytreesandbst;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class P437PathSumIII {
+    // O(n) approach
+    int totalPaths = 0; // Global
+
+    public int pathSum(TreeNode root, int sum) {
+        if (root == null) return 0;
+
+        Map<Integer, Integer> gainMap = new HashMap<>();
+        gainMap.put(0, 1);
+
+        findPathSum(root, sum, 0, gainMap);
+
+        return totalPaths;
+    }
+
+    public void findPathSum(TreeNode root, int sum, int currSum, Map<Integer, Integer> gainMap) {
+        if (root == null) return;
+
+        currSum += root.val;
+
+        if (gainMap.containsKey(currSum - sum)) {
+            totalPaths += gainMap.get(currSum - sum);
+        }
+
+        gainMap.put(currSum, gainMap.getOrDefault(currSum, 0) + 1);
+
+        findPathSum(root.left, sum, currSum, gainMap);
+        findPathSum(root.right, sum, currSum, gainMap);
+
+        // To reduce currSum count while backtracking
+        gainMap.put(currSum, gainMap.get(currSum) - 1);
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    
+    
+    /*
+    // O(n^2) approach
+    int totalPaths = 0; // Global
+    public int pathSum(TreeNode root, int sum) {
+        if(root == null) return 0;
+        
+        findPathSum(root, sum, 0);
+        // Perform the same operation for child nodes also.
+        pathSum(root.left, sum); 
+        pathSum(root.right, sum);
+        
+        return totalPaths;
+    }
+    
+    public void findPathSum(TreeNode root, int sum, int currSum){
+        if(root == null) return;
+        
+        currSum += root.val;
+        
+        if(currSum == sum) totalPaths++;
+        
+        findPathSum(root.left, sum, currSum);
+        findPathSum(root.right, sum, currSum);
+    }
+    */
+}
