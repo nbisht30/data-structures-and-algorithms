3721d4619348298bf74dec12fd70cf5e95acf939
nbisht30 <nbisht30@gmail.com>
2020-08-16 09:56:24 +0530

    LC - Contest | P115 Minimum Operations To Make Array Equal

---
+++ src/leetcode/contests/P1551MinimumOperationsToMakeArrayEqual.java
@@ -0,0 +1,27 @@
+package leetcode.contests;
+
+class P1551MinimumOperationsToMakeArrayEqual {
+    public int minOperations(int n) {
+        if (n % 2 == 1) {
+            int mid = n / 2;
+            int valMid = (mid * 2) + 1;
+            int i = 0;
+            int opn = 0;
+            while (i < mid) {
+                opn += (valMid - ((2 * i) + 1));
+                i++;
+            }
+            return opn;
+        } else {
+            int mid = (n / 2) - 1;
+            int valMid = ((mid * 2) + 1) + 1;
+            int opn = 1;
+            int i = 0;
+            while (i < mid) {
+                opn += (valMid - ((2 * i) + 1));
+                i++;
+            }
+            return opn;
+        }
+    }
+}
