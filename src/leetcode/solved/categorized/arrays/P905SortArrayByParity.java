1a8e412a966d4e86d2d3c7609ce3a8dcae01e493
nbisht30 <nbisht30@gmail.com>
2020-08-21 13:17:06 +0530

    LC - P905 | Sort array by parity

---
+++ src/leetcode/solved/categorized/arrays/P905SortArrayByParity.java
@@ -0,0 +1,17 @@
+package leetcode.solved.categorized.arrays;
+
+class P905SortArrayByParity {
+    public int[] sortArrayByParity(int[] A) {
+        // One pass solution, O(n) space
+        
+        int arr []  =new int[A.length];
+        int evenI = 0;
+        int oddI = A.length - 1;
+        for(int i : A){
+            if(i % 2 == 0) arr[evenI++] = i;
+            else arr[oddI--] = i;
+        }
+        
+        return arr;
+    }
+}
