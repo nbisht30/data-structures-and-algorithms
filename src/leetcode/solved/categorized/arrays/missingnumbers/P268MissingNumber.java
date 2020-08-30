8352641dbe32a0771a25b37774d620046657a84e
nbisht30 <nbisht30@gmail.com>
2020-08-30 17:45:17 +0530

    LC - P268 | Missing Number

---
+++ src/leetcode/solved/categorized/arrays/missingnumbers/P268MissingNumber.java
@@ -0,0 +1,11 @@
+package leetcode.solved.categorized.arrays.missingnumbers;
+
+class P268MissingNumber {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int expSum = n * (n + 1) / 2;
+        int sum = 0;
+        for(int i : nums) sum += i;
+        return expSum - sum;
+    }
+}
