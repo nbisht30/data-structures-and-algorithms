913aa933ebfc8f2d1078665ed978036d3ae3c5b6
nbisht30 <nbisht30@gmail.com>
2020-09-11 17:55:32 +0530

    LC - P152 | Maximum product subarray

---
+++ src/leetcode/solved/categorized/dp/kadanes/P152MaximumProductSubarray.java
@@ -0,0 +1,33 @@
+package leetcode.solved.categorized.dp.kadanes;
+
+class P152MaximumProductSubarray {
+    // Problem: https://leetcode.com/problems/maximum-product-subarray/
+    // Intuition: https://medium.com/@aliasav/algorithms-maximum-product-subarray-b09e520b4baf
+    /*
+    Explanation:-
+    We approach this problem bottom-up, such that at each iteration we will have the max possible subarray product.
+
+    Consider this, at each iteration over the nums array, the max product can be obtained:
+
+          - by multiplying nums[i] to a previous max product(max product till i-1th element)
+
+          - by multiplying nums[i] to a previous min product(min product till i-1th element)
+            (since both these nums[i] and previous min product might be negative,
+             it’s worthwhile to compute this value)
+
+          - nums[i] might itself be the largest
+     */
+
+    public int maxProduct(int[] nums) {
+        if (nums.length == 1) return nums[0];
+        int prevMin = nums[0], prevMax = nums[0], res = nums[0];
+        int temp;
+        for (int i = 1; i < nums.length; i++) {
+            temp = prevMin; // temporarily store prevMin to calculate prevMax later
+            prevMin = Math.min(nums[i], Math.min(nums[i] * prevMin, nums[i] * prevMax));
+            prevMax = Math.max(nums[i], Math.max(nums[i] * temp, nums[i] * prevMax));
+            res = Math.max(res, prevMax);
+        }
+        return res;
+    }
+}
---
+++ src/leetcode/solved/categorized/math/arithmetics/P7ReverseInteger.java
@@ -0,0 +1,21 @@
+package leetcode.solved.categorized.math.arithmetics;
+
+class Solution {
+    public int reverse(int x) {
+        int rev = 0;
+        int lstDig = 0;
+        while(x != 0){
+            lstDig = x % 10;
+            x /= 10;
+            if(rev > Integer.MAX_VALUE/10 || 
+               (rev == Integer.MAX_VALUE/10 && lstDig > 7))
+                return 0;
+            if(rev< Integer.MIN_VALUE/10 || 
+               (rev == Integer.MIN_VALUE/10 && lstDig < -8)) 
+                return 0;
+            rev *= 10;
+            rev += lstDig;
+        }
+        return rev;
+    }
+}
