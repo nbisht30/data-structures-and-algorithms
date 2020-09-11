2cecd8ebb6c7e5484a37ae7f8e9805eee0a3445e
nbisht30 <nbisht30@gmail.com>
2020-09-11 22:14:31 +0530

    LC - P198 | House Robber

---
+++ src/leetcode/solved/categorized/dp/P198HouseRobber.java
@@ -0,0 +1,15 @@
+package leetcode.solved.categorized.dp;
+
+class P198HouseRobber {
+    public int rob(int[] nums) {
+        if(nums.length == 0) return 0; // if no house then nums is empty
+        if(nums.length == 1) return nums[0];
+        int dp[] = new int[nums.length];
+        dp[0] = nums[0]; // if one house
+        dp[1] = Math.max(nums[0], nums[1]); // if two houses exist 
+        for(int i = 2; i < nums.length; i++){
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+        }
+        return dp[nums.length - 1];
+    }
+}
