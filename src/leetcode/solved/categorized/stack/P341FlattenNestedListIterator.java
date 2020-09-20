fdff6fcf51afa4fca8b3d1c880585ecf12eda963
nbisht30 <nbisht30@gmail.com>
2020-09-20 07:54:26 +0530

    LC - P341 | Flatten Nested List Iterator

---
+++ src/leetcode/solved/categorized/stack/P341FlattenNestedListIterator.java
@@ -0,0 +1,51 @@
+package leetcode.solved.categorized.stack;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Stack;
+
+interface NestedInteger {
+    public boolean isInteger();
+
+    public Integer getInteger();
+
+    public List<NestedInteger> getList();
+}
+
+public class P341FlattenNestedListIterator implements Iterator<Integer> {
+
+    Stack<NestedInteger> stack;
+
+    public P341FlattenNestedListIterator(List<NestedInteger> nestedList) {
+        stack = new Stack();
+        prepareStack(nestedList);
+    }
+
+    @Override
+    public Integer next() { // Return the top of stack which will always be an integer
+        return hasNext() ? stack.pop().getInteger() : null;
+    }
+
+    @Override
+    public boolean hasNext() {
+        while (!stack.isEmpty()) {
+            if (stack.peek().isInteger()) return true;
+            List<NestedInteger> list = stack.pop().getList();
+            prepareStack(list);
+        }
+
+        return !stack.isEmpty();
+    }
+
+    private void prepareStack(List<NestedInteger> list) { // Push in stack from back to front.
+        for (int i = list.size() - 1; i >= 0; i--) {
+            this.stack.push(list.get(i));
+        }
+    }
+}
+
+/**
+ * Your NestedIterator object will be instantiated and called as such:
+ * NestedIterator i = new NestedIterator(nestedList);
+ * while (i.hasNext()) v[f()] = i.next();
+ */
