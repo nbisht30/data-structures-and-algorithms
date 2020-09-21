a6953c5ae967fefbfb101a12af0997118953338b
nbisht30 <nbisht30@gmail.com>
2020-09-21 09:15:50 +0530

    LC - P42 | Trapping Rain Water

---
+++ src/leetcode/solved/categorized/arrays/P42TrappingRainWater.java
@@ -0,0 +1,48 @@
+package leetcode.solved.categorized.arrays;
+
+class P42TrappingRainWater {
+    // @TODO: Understand the O(1) space two-pointer solution.
+
+    public int trap(int[] height) {
+        if(height.length == 0) return 0;
+        int maxFromSt[] = new int[height.length];
+        int maxFromEnd[] = new int[height.length];
+        int rw = 0;
+        maxFromSt[0] = height[0];
+        maxFromEnd[height.length - 1] = height[height.length - 1];
+        for(int i = 1, j = height.length -  2; i < height.length; i++, j--){
+            maxFromSt[i] = Math.max(maxFromSt[i - 1], height[i]);
+            maxFromEnd[j] = Math.max(maxFromEnd[j + 1], height[j]);
+        }
+        
+        for(int i = 0; i < height.length; i++){
+            rw += Math.min(maxFromSt[i], maxFromEnd[i]) - height[i];
+        }
+            
+        return rw;
+    }
+
+    /*
+    Still trouble in understanding this: @TODO : Revisit
+     */
+    public int trapOptimizedSpace(int[] height) {
+        if(height.length == 0) return 0;
+        int leftMax = Integer.MIN_VALUE;
+        int rightMax = Integer.MIN_VALUE;
+        int rw = 0, left = 0, right = height.length -  1;
+        while(left < right){
+            if(height[left] < height[right]){ // if we have smaller element at left.
+                if(height[left] > leftMax) leftMax = height[left]; // If a new max from left is
+                    //found just store it in leftMax
+                else rw += leftMax - height[left];
+                left++;
+            }else{ // height[right] <= height[left]
+                if(height[right] > rightMax) rightMax = height[right];// If a new max from left is
+                    //found just store it in leftMax
+                else rw += rightMax - height[right];
+                right--;
+            }
+        }
+        return rw;
+    }
+}
--- src/leetcode/solved/categorized/hashing/P36ValidSudoku.java
+++ src/leetcode/solved/categorized/hashing/P36ValidSudoku.java
@@ -1,53 +1,48 @@
 package leetcode.solved.categorized.hashing;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 class P36ValidSudoku {
     // MYSELF
     // Optimization 1: Use boolean array instead of sets. In earlier submissions I used set.
     // @TODO: Optimize
     public boolean isValidSudoku(char[][] board) {
-        List<boolean[]> subBoxSets = fillBooleanList(3);
-        List<boolean[]> colsSets = fillBooleanList(9);
-        boolean[] row = new boolean[10];
+        List<boolean[]> subBoxCheckList = fillBooleanList(3);
+        List<boolean[]> colmsCheckList = fillBooleanList(9);
+        boolean[] row;
 
-        for(int i = 0; i < board.length; i++){
+        for (int i = 0; i < board.length; i++) {
             row = new boolean[10];
-            for(int j = 0; j < board.length; j++){
-                if(board[i][j] != '.') {
-                    if(row[board[i][j] - 48]) {
+            for (int j = 0; j < board.length; j++) {
+                if (board[i][j] != '.') {
+                    if (row[board[i][j] - 48]) {
                         return false;
-                    }
-                    else row[board[i][j] - 48] = true;
+                    } else row[board[i][j] - 48] = true;
 
-                    if(colsSets.get(j)[board[i][j] - 48]) {
+                    if (colmsCheckList.get(j)[board[i][j] - 48]) {
                         return false;
-                    }
-                    else colsSets.get(j)[board[i][j] - 48] = true;
+                    } else colmsCheckList.get(j)[board[i][j] - 48] = true;
 
                     int subBoxIndex = (j / 3);
-                    if(subBoxSets.get(subBoxIndex)[board[i][j] - 48]) {
+                    if (subBoxCheckList.get(subBoxIndex)[board[i][j] - 48]) {
                         return false;
-                    }
-                    else {
-                        subBoxSets.get(subBoxIndex)[board[i][j] - 48] = true;
+                    } else {
+                        subBoxCheckList.get(subBoxIndex)[board[i][j] - 48] = true;
                     }
                 }
             }
-            if((i + 1) % 3 == 0) subBoxSets = fillBooleanList(3);
+            if ((i + 1) % 3 == 0) subBoxCheckList = fillBooleanList(3);
         }
         return true;
     }
 
-    private List<boolean[]> fillBooleanList(int numSets){
-        List<boolean[]> listOfSets = new ArrayList<>();
-        while(numSets != 0){
-            listOfSets.add(new boolean[10]);
-            numSets--;
+    private List<boolean[]> fillBooleanList(int size) {
+        List<boolean[]> list = new ArrayList<>();
+        while (size != 0) {
+            list.add(new boolean[10]);
+            size--;
         }
-        return listOfSets;
+        return list;
     }
 }
