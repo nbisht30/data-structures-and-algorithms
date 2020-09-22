9b6f07630d938df7549d85589b4eb84445f4fd58
nbisht30 <nbisht30@gmail.com>
2020-09-22 09:14:33 +0530

    LC - P131 | Palindrome Partitioning | DP

--- src/leetcode/solved/categorized/backtracking/P131PalindromePartitioning.java
+++ src/leetcode/solved/categorized/backtracking/P131PalindromePartitioning.java
@@ -4,36 +4,89 @@ import java.util.ArrayList;
 import java.util.List;
 
 class P131PalindromePartitioning {
+    /*
+    Plain backtracking solution!
+    Problem: Performs one extra iteration to determine if a given substring is a palindrome or not.
+    Here, we are repeatedly iterating over the same substring multiple times and the result is always the same.
+     */
     public List<List<String>> partition(String s) {
         List<List<String>> result = new ArrayList<>();
-        findAll(s, result, new ArrayList<>());
+        boolean[][] palindrome = new boolean[s.length()][s.length()];
+        findAll(s, 0, result, new ArrayList<>());
         return result;
     }
 
-    /*
-    Plain backtracking solution!
-     */
-    void findAll(String s, List<List<String>> result, List<String> temp) {
+    void findAll(String s, int start, List<List<String>> result, List<String> temp) {
 
-        if (s.length() == 0) {
+        if (start >= s.length()) {
             result.add(new ArrayList<>(temp));
         }
 
-        for (int i = 0; i < s.length(); i++) {
-            String left = s.substring(0, i + 1);
-            if (isPalindrome(left)) {
+        for (int i = start; i < s.length(); i++) {
+            if (isPalindrome(s, start, i)) {
+                temp.add(s.substring(start, i + 1));
+                findAll(s, i + 1, result, temp);
+                temp.remove(temp.size() - 1); // Backtracking!!
+            }
+        }
+    }
+
+    boolean isPalindrome(String s, int start, int end) {
+        for (int i = start, j = end; i < j; i++, j--)
+            if (s.charAt(i) != s.charAt(j)) return false;
+        return true;
+    }
+
+
+    public List<List<String>> partitionDP(String s) {
+        List<List<String>> result = new ArrayList<>();
+        boolean[][] palindrome = new boolean[s.length()][s.length()];
+        findAllDP(s, 0, result, new ArrayList<>(), palindrome);
+        return result;
+    }
+
+    void findAllDP(String s, int start, List<List<String>> result, List<String> temp, boolean[][] palindrome) {
+
+        if (start >= s.length()) {
+            result.add(new ArrayList<>(temp));
+        }
+
+        for (int i = start; i < s.length(); i++) {
+            if (s.charAt(start) == s.charAt(i) && (i - start <= 2 || palindrome[start + 1][i - 1])) {
+                palindrome[start][i] = true;
+                temp.add(s.substring(start, i + 1));
+                findAll(s, i + 1, result, temp, palindrome);
+                temp.remove(temp.size() - 1); // Backtracking!!
+            }
+        }
+    }
+
+    /*
+    // My old backtracking impl. insteading of just using one string creating separte strings in left and right
+    // was getting very time consuming, above implementation is faster.
+    void findAll(String s, int start, List<List<String>> result, List<String> temp){
+
+        if(s.length() == 0) {
+            result.add(new ArrayList<>(temp));
+        }
+
+        for(int i = start; i < s.length(); i++){
+            String left = s.substring(start, i + 1);
+            if(isPalindrome(left)){
                 temp.add(left);
                 String right = s.substring(i + 1, s.length());
-                findAll(right, result, temp);
+                findAll(right, start, result, temp);
                 temp.remove(temp.size() - 1); // Backtracking!!
             }
 
         }
     }
 
-    boolean isPalindrome(String s) {
-        for (int i = 0, j = s.length() - 1; i < j; i++, j--)
-            if (s.charAt(i) != s.charAt(j)) return false;
+    boolean isPalindrome(String s){
+        for(int i = 0, j = s.length() - 1; i < j; i++, j--)
+            if(s.charAt(i) != s.charAt(j)) return false;
         return true;
     }
+    */
+
 }
