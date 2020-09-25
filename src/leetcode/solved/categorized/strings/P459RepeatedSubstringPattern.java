7902f7ce6f120ce3f2b50cef047e89e738091677
nbisht30 <nbisht30@gmail.com>
2020-09-25 07:46:47 +0530

    LC - P459 | Repeated Substring Pattern | O(n2) solution

--- src/leetcode/solved/categorized/strings/P459RepeatedSubstringPattern.java
+++ src/leetcode/solved/categorized/strings/P459RepeatedSubstringPattern.java
@@ -24,4 +24,25 @@ class P459RepeatedSubstringPattern {
         }
         return false;
     }
+
+        public boolean repeatedSubstringPatternNSquareSolution(String s) {
+            int N = s.length();
+
+            for(int i = N / 2; i >= 1; i--){ // starting i from N/2 because we compare from middle, i.e if s = ababab. Then we first compare aba and bab. Since they dont compare
+                // we do i-- and i becomes 2 and we compare ab with other two ab strings.
+                if(N % i == 0){ // if current string of length i can be repeated some n number of times to make original string i.e. s
+                    String subString = s.substring(0, i); // get the current substring eg. in ababab, get ab when i = 2. substring(0, i) means get i characters from 0th index including 0.
+                    int numRepeats = N / i; // get the number of times substring should repeat.
+                    StringBuilder sb = new StringBuilder();
+                    // construct a new string by repeat the substring numRepeats times.
+                    for(int j = 0; j < numRepeats; j++){
+                        sb.append(subString);
+                    }
+                    // compare if equal and return true
+                    if(sb.toString().equals(s)) return true;
+                }
+            }
+
+            return false;
+        }
 }
