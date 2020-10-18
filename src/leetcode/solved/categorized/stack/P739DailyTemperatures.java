14afb049d24d950c2524d7b9506a68bb609de70e
nbisht30 <nbisht30@gmail.com>
2020-10-18 06:55:45 +0530

    LC - P739. Daily Temperatures

--- src/leetcode/solved/categorized/stack/P739DailyTemperatures.java
+++ src/leetcode/solved/categorized/stack/P739DailyTemperatures.java
@@ -1,17 +1,19 @@
 package leetcode.solved.categorized.stack;
 
-class Solution {
+import java.util.Stack;
+
+class P739DailyTemperatures {
     public int[] dailyTemperatures(int[] T) {
         int N = T.length;
         int[] days = new int[N];
         Stack<Integer> indexStack = new Stack<>();
         indexStack.push(N - 1); // Put the last element on the stack
-        for(int i = N - 1; i >= 0; i--){
-            while(!indexStack.isEmpty() && T[indexStack.peek()] <= T[i]) indexStack.pop();
-            if(!indexStack.isEmpty()) {
+        for (int i = N - 1; i >= 0; i--) {
+            while (!indexStack.isEmpty() && T[indexStack.peek()] <= T[i]) indexStack.pop();
+            if (!indexStack.isEmpty()) {
                 days[i] = indexStack.peek() - i;
                 indexStack.push(i);
-            }else{
+            } else {
                 days[i] = 0;
                 indexStack.push(i);
             }
