2d215f0dd5b01153b2ee30288be1fc9de83ebdf5
nbisht30 <nbisht30@gmail.com>
2020-10-18 06:55:16 +0530

    P448. Find All Numbers Disappeared in an Array

---
+++ src/leetcode/solved/categorized/strings/P763PartitionLabels.java
@@ -0,0 +1,27 @@
+package leetcode.solved.categorized.strings;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public static void main(String[] args) {
+        System.out.println(partitionLabels("ababcbacadefegdehijhklij"));
+    }
+
+    public static List<Integer> partitionLabels(String S) {
+        int[] last = new int[26];
+        for (int i = 0; i < S.length(); ++i)
+            last[S.charAt(i) - 'a'] = i;
+
+        int j = 0, anchor = 0;
+        List<Integer> ans = new ArrayList();
+        for (int i = 0; i < S.length(); ++i) {
+            j = Math.max(j, last[S.charAt(i) - 'a']);
+            if (i == j) {
+                ans.add(i - anchor + 1);
+                anchor = i + 1;
+            }
+        }
+        return ans;
+    }
+}
