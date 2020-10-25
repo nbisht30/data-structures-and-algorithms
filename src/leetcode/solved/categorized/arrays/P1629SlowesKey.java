c283df20f93946a9a7e02afae5c7d0e052e555b5
nbisht30 <nbisht30@gmail.com>
2020-10-25 10:37:26 +0530

    LC - P1629. Slowest Key

---
+++ src/leetcode/solved/categorized/arrays/P1629SlowesKey.java
@@ -0,0 +1,18 @@
+package leetcode.solved.categorized.arrays;
+
+class P1629SlowesKey {
+    public char slowestKey(int[] releaseTimes, String keysPressed) {
+        if(releaseTimes.length == 1) return keysPressed.charAt(0);
+        int max = releaseTimes[0];
+        char c = keysPressed.charAt(0);
+        for(int i = 1; i < releaseTimes.length; i++){
+            if((releaseTimes[i] - releaseTimes[i - 1]) > max) {
+                max = releaseTimes[i] - releaseTimes[i - 1];
+                c = keysPressed.charAt(i);
+            }if((releaseTimes[i] - releaseTimes[i - 1]) == max && keysPressed.charAt(i) > c){
+                c = keysPressed.charAt(i);
+            }
+        }
+        return c;
+    }
+}
