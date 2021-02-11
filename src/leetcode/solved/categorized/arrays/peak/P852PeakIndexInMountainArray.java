5ddcd171dfa35a1edb60942a0b592d0d0a718ec0
nbisht30 <nbisht30@gmail.com>
2021-02-11 17:34:31 +0530

    LC - P    }

--- src/leetcode/solved/categorized/arrays/peak/P852PeakIndexInMountainArray.java
+++ src/leetcode/solved/categorized/arrays/peak/P852PeakIndexInMountainArray.java
@@ -7,13 +7,29 @@ class P852PeakIndexInMountainArray {
     public int peakIndexInMountainArray(int[] A) {
         int peak = Integer.MIN_VALUE;
         int ind = 0;
-        for(int i = 0; i < A.length; i++) {
-            if(peak < A[i]) {
-            ind = i;
-            peak = A[i];
+        for (int i = 0; i < A.length; i++) {
+            if (peak < A[i]) {
+                ind = i;
+                peak = A[i];
             }
         }
-        
+
         return ind;
+    }
+
+    // O(N LogN)
+    public int peakIndexInMountainArrayBinarySearch(int[] arr) {
+
+        int lo = 0, hi = arr.length - 1;
+
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (mid == 0 || mid == arr.length - 1) return mid;
+            if (arr[mid] > arr[mid + 1] && arr[mid] > arr[mid - 1]) return mid;
+            else if (arr[mid] < arr[mid + 1]) lo = mid + 1;
+            else hi = mid;
+        }
+
+        return -1;
     }
 }
