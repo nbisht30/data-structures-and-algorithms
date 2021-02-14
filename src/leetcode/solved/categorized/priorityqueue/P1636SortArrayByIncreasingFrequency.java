0f46b9f81b19811ac75fbe72a0408dbf2153d7c3
nbisht30 <nbisht30@gmail.com>
2021-02-14 09:24:19 +0530

    LC - P1636. Sort Array by Increasing Frequency

--- src/leetcode/solved/categorized/priorityqueue/P1636SortArrayByIncreasingFrequency.java
+++ src/leetcode/solved/categorized/priorityqueue/P1636SortArrayByIncreasingFrequency.java
@@ -6,25 +6,22 @@ import java.util.PriorityQueue;
 
 class P1636SortArrayByIncreasingFrequency {
     public int[] frequencySort(int[] nums) {
-        HashMap<Integer, Integer> count = new HashMap();
-        for (int n : nums) {
-            count.put(n, count.getOrDefault(n, 0) + 1);
+        Map<Integer, Integer> count = new HashMap<>();
+        for(int i : nums){
+            count.put(i, count.getOrDefault(i, 0) + 1);
         }
 
-        PriorityQueue<Map.Entry<Integer, Integer>> heap = new PriorityQueue<>(
-                (a, b) -> a.getValue() == b.getValue() ? b.getKey().compareTo(a.getKey()) : a.getValue() - b.getValue()
-        );
+        PriorityQueue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>((a, b) -> a.getValue() == b.getValue() ? (b.getKey() - a.getKey()) : a.getValue() - b.getValue());
+        pq.addAll(count.entrySet());
 
-        for (Map.Entry<Integer, Integer> n : count.entrySet()) {
-            heap.add(n);
-        }
-
-        int j = 0;
-        while (!heap.isEmpty()) {
-            Map.Entry<Integer, Integer> entry = heap.poll();
-            int ele = entry.getKey();
-            int size = entry.getValue();
-            for (int i = 0; i < size; i++) nums[j++] = ele;
+        int currIndex = 0;
+        while(!pq.isEmpty()){
+            Map.Entry<Integer, Integer> entry = pq.poll();
+            int key = entry.getKey();
+            int keyCount = entry.getValue();
+            for(int i = 0; i < keyCount; i++){
+                nums[currIndex++] = key;
+            }
         }
         return nums;
     }
