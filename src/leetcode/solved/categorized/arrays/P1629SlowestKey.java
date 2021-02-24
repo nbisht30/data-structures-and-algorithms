d3bc62d73af236db1593b52b1b4a5f5145ce2471
nbisht30 <nbisht30@gmail.com>
2021-02-24 14:03:25 +0530

    LC - P1629. Slowest Key | Refractoring

--- src/leetcode/solved/categorized/arrays/P1629SlowestKey.java
+++ src/leetcode/solved/categorized/arrays/P1629SlowestKey.java
@@ -2,17 +2,17 @@ package leetcode.solved.categorized.arrays;
 
 class P1629SlowestKey {
     public char slowestKey(int[] releaseTimes, String keysPressed) {
-        if(releaseTimes.length == 1) return keysPressed.charAt(0);
-        int max = releaseTimes[0];
-        char c = keysPressed.charAt(0);
+        char maxKey = keysPressed.charAt(0);
+        int maxDur = releaseTimes[0];
         for(int i = 1; i < releaseTimes.length; i++){
-            if((releaseTimes[i] - releaseTimes[i - 1]) > max) {
-                max = releaseTimes[i] - releaseTimes[i - 1];
-                c = keysPressed.charAt(i);
-            }if((releaseTimes[i] - releaseTimes[i - 1]) == max && keysPressed.charAt(i) > c){
-                c = keysPressed.charAt(i);
+            int currDur = releaseTimes[i] - releaseTimes[i - 1];
+            if(currDur > maxDur) {
+                maxDur = currDur;
+                maxKey = keysPressed.charAt(i);
+            } else if(currDur == maxDur && maxKey < keysPressed.charAt(i)) {
+                maxKey = keysPressed.charAt(i);
             }
         }
-        return c;
+        return maxKey;
     }
 }
