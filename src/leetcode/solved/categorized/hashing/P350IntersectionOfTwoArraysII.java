1810f188037eae8767d391d97ce0c4f80f744e8d
nbisht30 <nbisht30@gmail.com>
2021-03-12 10:21:29 +0530

    LC - P350. Intersection of Two Arrays II | Resolved

--- src/leetcode/solved/categorized/hashing/P350IntersectionOfTwoArraysII.java
+++ src/leetcode/solved/categorized/hashing/P350IntersectionOfTwoArraysII.java
@@ -6,32 +6,30 @@ import java.util.List;
 import java.util.Map;
 
 class P350IntersectionOfTwoArraysII {
-    //MYSELF
-    //TIME: 13 mins
-    //APPROACH: HASHING
+    // Myself
+    // Time: 13 mins
+    // Approach: HASHING
+    // Time(Second Attempt): 19:20 mins(Got stuck with using sets)
+
     public int[] intersect(int[] nums1, int[] nums2) {
-        Map<Integer, Integer> map = new HashMap<>();
-
-        for (int i : nums1) {
-            map.put(i, map.getOrDefault(i, 0) + 1);
+        HashMap<Integer, Integer> map= new HashMap<>();
+        for(int item : nums1) {
+            map.put(item, map.getOrDefault(item, 0) + 1);
         }
-
-        int size = nums1.length > nums2.length ? nums2.length : nums1.length;
-        List<Integer> list = new ArrayList<>(size);
-        for (int i : nums2) {
-            if (map.containsKey(i)) {
-                list.add(i);
-                if (map.get(i) > 1) map.put(i, map.get(i) - 1);
-                else map.remove(i);
+        int maxSize = nums1.length > nums2.length ? nums1.length : nums2.length, count = 0;
+        int[] maxSizeArr = new int[maxSize];
+        for(int item : nums2) {
+            if(map.containsKey(item)){
+                maxSizeArr[count++] = item;
+                map.put(item, map.get(item) - 1);
+                if(map.get(item) <= 0) map.remove(item);
             }
         }
 
-        int arr[] = new int[list.size()];
-        int i = 0;
-        for (int k : list) {
-            arr[i++] = k;
+        int[] intersection = new int[count];
+        for(int i = 0; i < count; i++){
+            intersection[i] = maxSizeArr[i];
         }
-
-        return arr;
+        return intersection;
     }
 }
