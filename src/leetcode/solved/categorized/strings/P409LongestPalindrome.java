08bb019c71800ae4886e73e02225d3ed6586ac17
nbisht30 <nbisht30@gmail.com>
2021-03-19 16:48:02 +0530

    LC - P409 - Longest Palindrome | Cleaned

--- src/leetcode/solved/categorized/strings/P409LongestPalindrome.java
+++ src/leetcode/solved/categorized/strings/P409LongestPalindrome.java
@@ -1,31 +1,22 @@
 package leetcode.solved.categorized.strings;
 
 class P409LongestPalindrome {
+
     public int longestPalindrome(String s) {
-        /*
-        d - 2,  a - 1, c - 4, b - 1
-        even - > on two opposite ends of the string.    
-        */
-        
-        int[] count = new int [128];
-        
-        char[] str = s.toCharArray();
-        
-        for(char ch : str) count[ch]++;
-        
-        int maxLen = 0;
-        boolean oddFound = false;
-        for(int i : count){
-            if(i != 0){
-                if(i % 2 == 0) maxLen += i;
-                else{
-                    oddFound = true;
-                    maxLen += (i - 1);
-                }
+        int[] count = new int[128];
+        for(int i = 0; i < s.length(); i++){
+            count[s.charAt(i)]++;
+        }
+
+        int len = 0;
+
+        for(int i = 0; i < 128; i++){
+            len += (count[i] / 2) * 2;
+            if(len % 2 == 0 && count[i] % 2 == 1){
+                len++;
             }
         }
-        maxLen +=  oddFound ? 1 : 0;
-        
-        return maxLen;
+
+        return len;
     }
 }
