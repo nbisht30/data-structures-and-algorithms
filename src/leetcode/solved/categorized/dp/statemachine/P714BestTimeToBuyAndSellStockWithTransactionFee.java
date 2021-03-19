02ef22a1c01b537663c2332469bec6ee16e6b8c7
nbisht30 <nbisht30@gmail.com>
2021-03-19 07:13:00 +0530

    LC - P714. Best Time to Buy and Sell Stock with Transaction Fee

--- src/leetcode/solved/categorized/dp/statemachine/P714BestTimeToBuyAndSellStockWithTransactionFee.java
+++ src/leetcode/solved/categorized/dp/statemachine/P714BestTimeToBuyAndSellStockWithTransactionFee.java
@@ -4,6 +4,7 @@ class P714BestTimeToBuyAndSellStockWithTransactionFee {
 
     // https://www.thealgorists.com/Algo/DynamicProgramming/StateMachine/StockTradingWithTransactionFee
     // https://www.youtube.com/watch?v=pTQB9wbIpfU&ab_channel=Pepcoding
+    // Also check notes.
     public int maxProfit(int[] prices, int fee) {
         int hasStock = -prices[0]; // maximum profit achievable at the end of day i if we have a stock with us on end of day i.
         int noStock = 0; // maximum profit achievable at the end of day i if we have no stock with us on end of day i.
@@ -14,7 +15,16 @@ class P714BestTimeToBuyAndSellStockWithTransactionFee {
             - By doing nothing, i.e, transition from hasStock[i - 1].
                 hasStock[i] = hasStock[i - 1]
             - From noStock[i - 1]: by buying one stock.
-                hasStock[i] = noStock[i - 1] - prices[i] (Max profit/money without stock - money for buying day i stock)
+                hasStock[i] = noStock[i - 1] - prices[i]
+
+            i.e.
+            Max profit achievable at the end of day i if we have a stock with us on end of day i
+             = Max((Max profit by holding the existing stock and not buying the current stock)
+             OR (Max profit after buying current stock if not holding any stock earlier))
+
+            Where, Max profit after buying current stock if not holding any stock earlier =
+            Max profit/money without any stock(noStock) -
+            money for buying day i stock(Subtracting because buying current stock from noStock money))
             */
             hasStock = Math.max(hasStock, noStock - prices[i]);
             
@@ -24,6 +34,17 @@ class P714BestTimeToBuyAndSellStockWithTransactionFee {
                 noStock[i] = noStock[i - 1]
             - From hasStock[i - 1] by selling stock and paying transaction fee.
                 noStock[i] = hasStock[i - 1] + prices[i] - fee
+
+            i.e
+            Max profit achievable at the end of day i if we have no stock with us on end of day i =
+            Max((Max profit by not buying any stock on day i and continuing with existing profit)
+             OR (Max profit after selling existing stock, if holding any and paying the transaction fee))
+
+            Where, Max profit after selling existing stock, if holding any and paying the transaction fee =
+            Max profit with the existing stock + Price of the day i stock - Transaction fee
+            (Since hasStock is initially started with - prices[0], i.e. it can be negative
+            on adding prices[i], we actually calculate the profit by
+            selling the existing stock
             */
             noStock = Math.max(noStock, hasStock + prices[i] - fee);
            
