463b06dfb339066ef7a9d0ab026a1350a2773502
nbisht30 <nbisht30@gmail.com>
2021-04-27 08:06:48 +0530

    LC - P70. Climbing Stairs | Refactored | Second Attempt

--- src/leetcode/solved/categorized/dp/P70ClimbingStairs.java
+++ src/leetcode/solved/categorized/dp/P70ClimbingStairs.java
@@ -4,20 +4,20 @@ class P70ClimbingStairs {
     //MYSELF
     //TIME: 9 MIN
     //APPROACH: DYNAMIC PROGRAMMING
+
+    //Time(Second Attempt: 27/Apr/21) : 6 Mins
     public int climbStairs(int n) {
         return helper(n);
     }
 
     public int helper(int n) {
-        if (n == 1) return 1;
-        if (n == 2) return 2;
-        int a[] = new int[n + 1];
-        a[0] = 0;
-        a[1] = 1;
-        a[2] = 2;
-        for (int i = 3; i <= n; i++) {
-            a[i] = a[i - 1] + a[i - 2];
+        if(n == 1) return 1;
+        int dp[] = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
-        return a[n];
+        return dp[n];
     }
 }
