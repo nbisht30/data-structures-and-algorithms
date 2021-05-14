aaa485c4e5e5ac9187fe8723210a29cddcd595ae
nbisht30 <nbisht30@gmail.com>
2021-05-14 18:20:16 +0530

    LC - P7. Reverse Integer

--- src/leetcode/solved/categorized/math/arithmetics/P7ReverseInteger.java
+++ src/leetcode/solved/categorized/math/arithmetics/P7ReverseInteger.java
@@ -5,18 +5,38 @@ class P7ReverseInteger {
     public int reverse(int x) {
         int rev = 0;
         int lstDig = 0;
-        while(x != 0){
+        while (x != 0) {
             lstDig = x % 10;
             x /= 10;
-            if(rev > Integer.MAX_VALUE/10 || 
-               (rev == Integer.MAX_VALUE/10 && lstDig > 7))
+            if (rev > Integer.MAX_VALUE / 10 ||
+                    (rev == Integer.MAX_VALUE / 10 && lstDig > 7))
                 return 0;
-            if(rev< Integer.MIN_VALUE/10 || 
-               (rev == Integer.MIN_VALUE/10 && lstDig < -8)) 
+            if (rev < Integer.MIN_VALUE / 10 ||
+                    (rev == Integer.MIN_VALUE / 10 && lstDig < -8))
                 return 0;
             rev *= 10;
             rev += lstDig;
         }
         return rev;
+    }
+
+    // MYSELF
+    // TIME: 10 mins
+    // DATE: 14-May-21
+    // COMMENTS: Much cleaner solution
+    public int reverseSecondAttempt(int x) {
+
+        boolean neg = x < 0 ? true : false;
+        x = Math.abs(x);
+        int res = 0;
+
+        while (x > 0) {
+            int last = x % 10;
+            if (res > (Integer.MAX_VALUE - last) / 10) return 0;
+            res = res * 10 + last;
+            x /= 10;
+        }
+
+        return neg ? -res : res;
     }
 }
