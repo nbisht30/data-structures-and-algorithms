08e63a8fc5b68f666c84a874806400f447322f75
nbisht30 <nbisht30@gmail.com>
2021-05-19 13:34:28 +0530

    LC - P763. Partition Labels

--- src/leetcode/solved/categorized/strings/P763PartitionLabels.java
+++ src/leetcode/solved/categorized/strings/P763PartitionLabels.java
@@ -1,6 +1,7 @@
 package leetcode.solved.categorized.strings;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 class P763PartitionLabels {
@@ -23,5 +24,43 @@ class P763PartitionLabels {
             }
         }
         return ans;
+    }
+
+    // MYSELF
+    // DATE: 19-05-2021, second attempt
+    // TIME: 17 mins
+    // COMMENT: Figured out the approach myself, this is best approach for this problem and is much more optimized
+    // than my approach last time https://leetcode.com/submissions/detail/409729744/
+
+    public List<Integer> partitionLabelsSecondAttempt(String s) {
+
+        // find the last occurrence of each character
+        int[] last = new int[26];
+        for(int i = 0; i < s.length(); i++) {
+            int index = s.charAt(i) - 'a';
+            last[index] = i;
+        }
+
+        // for each character keep a min first and max last and continue till the
+        // next characters lie between these minFirst and maxLast
+        // if current character's index is after the current maxLast(i > maxLast) then put the length
+        // in result list and update minFirst and maxLast.
+        List<Integer> res = new ArrayList<>();
+        int minFirst = 0, maxLast = 0;
+        for(int i = 0; i < s.length(); i++) {
+            int ind = s.charAt(i) - 'a';
+            if(i > maxLast) {
+                int len = maxLast - minFirst + 1;
+                res.add(len);
+                minFirst = i;
+                maxLast = last[ind];
+            }
+            else {
+                minFirst = Math.min(minFirst, i);
+                maxLast = Math.max(maxLast, last[ind]);
+            }
+        }
+        res.add(maxLast - minFirst + 1);
+        return res;
     }
 }
