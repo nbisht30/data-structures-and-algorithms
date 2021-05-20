0a9af765319c7f0d18bd3698fcae090a02028060
nbisht30 <nbisht30@gmail.com>
2021-05-20 21:26:04 +0530

    LC - P64. Minimum Path Sum

---
+++ src/leetcode/solved/categorized/graphs/P64MinimumPathSum.java
@@ -0,0 +1,28 @@
+package leetcode.solved.categorized.graphs;
+
+class P64MinimumPathSum {
+
+    // MYSELF
+    // DATE: 20-05-2021, first attempt
+    // TIME: 15 mins
+    public int minPathSum(int[][] grid) {
+        int[][] dp = new int[grid.length][grid[0].length];
+        return find(grid, 0, 0, dp);
+    }
+    
+    int find(int[][] grid, int i, int j, int[][] dp) {
+        
+        if(i < grid.length && j < grid[i].length && dp[i][j] > 0) return dp[i][j];
+        
+        if(i == grid.length || j == grid[i].length) return Integer.MAX_VALUE;
+        
+        if(i == grid.length - 1 && j == grid[i].length - 1) return grid[i][j];
+        
+        int right = find(grid, i + 1, j, dp);
+        int down = find(grid, i, j + 1, dp);
+        
+        dp[i][j] = Math.min(right, down) + grid[i][j];
+        
+        return dp[i][j];
+    }
+}
