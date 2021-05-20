09f977fc02c6a211dcbb987baefa23cf773ec36a
nbisht30 <nbisht30@gmail.com>
2021-05-20 22:54:08 +0530

    LC - P1630. Arithmetic Subarrays

--- src/leetcode/solved/categorized/arrays/P1630ArithmeticSubarrays.java
+++ src/leetcode/solved/categorized/arrays/P1630ArithmeticSubarrays.java
@@ -29,4 +29,49 @@ class P1630ArithmeticSubarrays {
         }
         return true;
     }
+
+    // EXPLANATION: https://leetcode.com/problems/arithmetic-subarrays/discuss/1220187/EXPLAINED-or-Java-or-Without-Sorting-or-Arithmetic-Progression
+    class WithoutSorting {
+        public List<Boolean> checkArithmeticSubarrays(int[] nums, int[] l, int[] r) {
+            List<Boolean> res = new ArrayList<>(l.length);
+            for(int i = 0; i < l.length; i++) {
+                int st = l[i], end = r[i];
+                res.add(checkArithmetic(nums, st, end));
+            }
+            return res;
+        }
+
+        private boolean checkArithmetic(int[] nums, int st, int end) {
+            int min = nums[st], max = nums[st];
+
+            // find min and max, ie. first and last terms of Arithmetic Progression
+            for(int i = st; i <= end; i++) {
+                min = Math.min(min, nums[i]);
+                max = Math.max(max, nums[i]);
+            }
+
+            int len = end - st + 1;
+
+            // if all elements are same
+            if(min == max) return true;
+
+            // Arithmetic Progression(AP) formula : An = Ao + (n - 1) * D => (n - 1) * D = An - Ao => D = (An - Ao) / (n - 1)
+            int commonDiff = (max - min) / (len - 1);
+
+            // if commonDiff was fractional i.e. AP was not evenly spaced
+            if((max - min) % (len - 1) != 0) return false;
+
+            boolean[] found = new boolean[len];
+            for(int i = st; i <= end; i++) {
+                int pos = (nums[i] - min) / commonDiff;
+                boolean posFractional = (nums[i] - min) % commonDiff != 0;
+                if(posFractional || found[pos]) return false;
+                found[pos] = true;
+            }
+
+            return true;
+        }
+    }
+
+
 }
