bf5ffec90f7d53c2337bd0bfbfa8d3b345ce09e1
nbisht30 <nbisht30@gmail.com>
2021-05-23 18:28:24 +0530

    LC - P1395

--- src/leetcode/solved/categorized/arrays/P1395CountNumberOfTeams.java
+++ src/leetcode/solved/categorized/arrays/P1395CountNumberOfTeams.java
@@ -12,17 +12,17 @@ class P1395CountNumberOfTeams {
     public int numTeams(int[] rating) {
         int N = rating.length;
         int count = 0;
-        for(int i = 0; i < N; i++){
-            for(int j = i + 1; j < N; j++){
-                if(rating[j] > rating[i]){
-                    for(int k = j + 1; k < N; k++){
-                        if(rating[k] > rating[j]){
+        for (int i = 0; i < N; i++) {
+            for (int j = i + 1; j < N; j++) {
+                if (rating[j] > rating[i]) {
+                    for (int k = j + 1; k < N; k++) {
+                        if (rating[k] > rating[j]) {
                             count++;
                         }
-                    }    
+                    }
                 } else if (rating[j] < rating[i]) {
-                    for(int k = j + 1; k < N; k++){
-                        if(rating[k] < rating[j]){
+                    for (int k = j + 1; k < N; k++) {
+                        if (rating[k] < rating[j]) {
                             count++;
                         }
                     }
@@ -31,5 +31,36 @@ class P1395CountNumberOfTeams {
         }
         return count;
     }
-    
+
+    public static void main(String[] args) {
+        System.out.println(numTeamsOptimized(new int[]{2, 5, 3, 4, 1}));
+    }
+
+    // NEEDED HELP
+    // DATE: 23-May-21, second attempt
+    // COMMENTS: Coded the brute force but it did not get accepted this time.
+    /* ALGORITHM O(N*N):-
+       For each soldier, count the number of soldiers with lesser ratings on the left(lessLeft) and greater ratings on the right(greaterRight).
+       and count the number of soldiers with greater ratings on the left(greaterLeft) and lesser ratings on the right(lessRight);
+       then number of teams using that soldier is = lessLeft * greaterRight + greaterLeft * lessRight;
+     */
+    public static int numTeamsOptimized(int[] rating) {
+        int numTeams = 0;
+        for (int mainSoldier = 1; mainSoldier < rating.length - 1; mainSoldier++) {
+
+            int lessLeft = 0, lessRight = 0, greaterLeft = 0, greaterRight = 0;
+
+            for (int currSoldier = 0; currSoldier < rating.length; currSoldier++) {
+                if (rating[currSoldier] > rating[mainSoldier]) {
+                    if (currSoldier < mainSoldier) greaterLeft++;
+                    else greaterRight++;
+                } else if (rating[currSoldier] < rating[mainSoldier]) {
+                    if (currSoldier < mainSoldier) lessLeft++;
+                    else lessRight++;
+                }
+            }
+            numTeams += lessLeft * greaterRight + greaterLeft * lessRight;
+        }
+        return numTeams;
+    }
 }
