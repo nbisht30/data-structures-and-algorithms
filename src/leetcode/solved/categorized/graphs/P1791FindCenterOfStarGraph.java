611c334790b13fb37b0d43db6494214b182666af
nbisht30 <nbisht30@gmail.com>
2021-06-02 08:26:05 +0530

    P1791

---
+++ src/leetcode/solved/categorized/graphs/P1791FindCenterOfStarGraph.java
@@ -0,0 +1,17 @@
+package leetcode.solved.categorized.graphs;
+
+class P1791FindCenterOfStarGraph {
+    // MYSELF
+    // DATE: 02-06-2021, first attempt
+    // TIME: 1 min
+
+    public int findCenter(int[][] edges) {
+        int[] e1 = edges[0];
+        int[] e2 = edges[1];
+
+        for (int v1 : e1) {
+            for (int v2 : e2) if (v1 == v2) return v1;
+        }
+        return -1;
+    }
+}
