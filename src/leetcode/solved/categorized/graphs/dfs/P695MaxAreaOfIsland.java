8d5fd49377d24c961cb20ee6192f6e703c5d5ab1
nbisht30 <nbisht30@gmail.com>
2021-06-05 11:13:33 +0530

    LC - P695 | Second Attempt

--- src/leetcode/solved/categorized/graphs/dfs/P695MaxAreaOfIsland.java
+++ src/leetcode/solved/categorized/graphs/dfs/P695MaxAreaOfIsland.java
@@ -30,4 +30,34 @@ class P695MaxAreaOfIsland {
         
         return up + down + left + right + 1;
     }
+
+
+    // MYSELF
+    // DATE: 05-06-2021, second attempt
+    // TIME: 9:27 mins
+    class SecondAttempt {
+
+        public int maxAreaOfIsland(int[][] grid) {
+            int maxArea = 0;
+            for(int i = 0; i < grid.length; i++) {
+                for(int j = 0; j < grid[i].length; j++) {
+                    if(grid[i][j] == 1) maxArea = Math.max(maxArea, dfs(grid, i, j));
+                }
+            }
+            return maxArea;
+        }
+
+        int dfs(int[][] grid, int i, int j) {
+            if(i < 0 || j < 0 || i == grid.length || j == grid[i].length || grid[i][j] < 1) return 0;
+
+            grid[i][j] = -1;
+
+            int up = dfs(grid, i + 1, j);
+            int down = dfs(grid, i - 1, j);
+            int right = dfs(grid, i, j + 1);
+            int left = dfs(grid, i, j - 1);
+
+            return up + down + left + right + 1;
+        }
+    }
 }
