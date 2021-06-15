0ca8bda65720002f5b5b7db1bc145f936fa7f986
nbisht30 <nbisht30@gmail.com>
2021-06-15 09:01:21 +0530

    LC - P822"

--- src/leetcode/solved/categorized/graphs/sssp/dijkstra/P882ReachableNodesInSubdividedGraph.java
+++ src/leetcode/solved/categorized/graphs/sssp/dijkstra/P882ReachableNodesInSubdividedGraph.java
@@ -6,37 +6,37 @@ import java.util.PriorityQueue;
 class P882ReachableNodesInSubdividedGraph {
     public static void main(String[] args) {
         P882ReachableNodesInSubdividedGraph s = new P882ReachableNodesInSubdividedGraph();
-        int[][] edges = {{0, 1, 10}, {0, 2, 1}, {1, 2, 2}};
-        int maxMoves = 6;
-        int n = 3;
+        int[][] edges = {{0, 1, 4}, {1, 2, 6}, {0, 2, 8}, {1, 3, 1}};
+//        int[][] edges = {{0, 1, 10}, {0, 2, 1}, {1, 2, 2}};
+        int maxMoves = 12;
+        int n = 4;
         int reachableNodes = s.reachableNodes(edges, maxMoves, n);
         System.out.println(reachableNodes);
     }
 
     public int reachableNodes(int[][] edges, int maxMoves, int n) {
-
         int[][] adjMatrix = getAdjMatrix(edges, n);
         int reachableCount = 0;
-
+        boolean[] visited = new boolean[n];
         PriorityQueue<Node> pq = new PriorityQueue<>
                 (Comparator.comparingInt(a -> a.distFromSrc));
         pq.add(new Node(0, 0));
 
         while (!pq.isEmpty()) {
             Node node = pq.poll();
-
             int[] neighbors = adjMatrix[node.index];
             for (int i = 0; i < neighbors.length; i++) {
                 int cost = neighbors[i], to = i, from = node.index;
 
                 if (cost <= 0) continue;
-                int nextDist = cost + 1;
+                int nextDist = cost;
+                if(!visited[to]) nextDist++; // +1 for counting to node also if it was not visited
                 int totalDist = node.distFromSrc + nextDist;
-                // +1 for counting edge.to node also
                 if (totalDist >= maxMoves) {
                     nextDist = maxMoves - node.distFromSrc;
                     cost -= nextDist;
                 } else {
+                    visited[to] = true;
                     cost = 0;
                     pq.offer(new Node(to, totalDist));
                 }
@@ -45,8 +45,8 @@ class P882ReachableNodesInSubdividedGraph {
                 reachableCount += nextDist;
             }
         }
-
-        return reachableCount + 1; // +1 since at least source node(0) is reachable
+        reachableCount++; // +1 since at least source node(0) is reachable
+        return reachableCount;
 
     }
 
